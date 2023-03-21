824998a22e7108d70452ce29add9f4fbcd4fcf50
R3ti0x <71370086+R3ti0x@users.noreply.github.com>
2023-03-21 10:21:45 +0100

    Add files via upload

--- consigne.c
+++ consigne.c
@@ -1,11 +1,28 @@
 #include "consigne.h"
+#include <unistd.h>
 
    float consigne(float thermostatPrec_f)
+   
    {
-   
-      float thermostat_f=0;
-   
-     
-      return thermostat_f;
-   
+      float thermostat=0;
+      FILE* verrou=NULL;
+      FILE* cons=NULL;
+
+      if( access( ".verrouConsigne", F_OK )!= -1){
+         thermostat = thermostatPrec_f;
+      }
+      else{
+         verrou=fopen(".verrouConsigne","w");
+         cons=fopen("consigne.txt","r+");
+         fscanf(cons, "%f" , &thermostat);
+         fclose(cons);
+         fclose(verrou);
+         remove(".verrouConsigne");
+      }
+      
+      printf("%f", thermostat);
+      return thermostat;
+      
    }
+
+   
